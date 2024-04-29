c4ecb62cef6e25e5c124ef53007e9ba76c6b2c90
Gadisa16 <133113137+Gadisa16@users.noreply.github.com>
2024-04-29 08:56:14 +0300

    Initialize React project and install necessary packages

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/header/Header.jsx';"../../components/header/Header.jsx";
+import Footer from "../../components/footer/Footer.jsx";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
