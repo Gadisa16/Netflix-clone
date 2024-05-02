4b0367fce1ef217e7623e212cde768dc42797bcb
Gadisa16 <133113137+Gadisa16@users.noreply.github.com>
2024-05-02 08:28:56 +0300

    Header and Footer are done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
+import "./footer.css"
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 
 function Footer() {
   return (
-    <div>
-        <h2>Footer</h2>
+    <div className="footer">
+      <div className="mat-icons">
+        <FacebookOutlinedIcon />
+        <InstagramIcon />
+        <YouTubeIcon />
+      </div>
+
+      <div className="FooterMainDiv">
+        <div className="div1">
+          <ul>
+            <li>Audio Description</li>
+            <li>Inverse Relations</li>
+            <li>Legal Notice</li>
+            <li>Service Code</li>
+            <li>&copy; 1997-2024 Netflix, Inc.</li>
+          </ul>
+        </div>
+
+        <div>
+          <ul>
+            <li>Help Center</li>
+            <li>Jobs</li>
+            <li>cookie Preferences</li>
+          </ul>
+        </div>
+
+        <div>
+          <ul>
+            <li>Gift Cards</li>
+            <li>Terms of Use</li>
+            <li>Corporate Information</li>
+          </ul>
+        </div>
+
+        <div>
+          <ul>
+            <li>Media Center</li>
+            <li>Privacy</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import './Header.css';
+import netflixLogo from '../../assets/netflixLogo.png';
+
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import AccountBoxOutlinedIcon from '@mui/icons-material/AccountBoxOutlined';
+import ArrowDropDownOutlinedIcon from '@mui/icons-material/ArrowDropDownOutlined';
 
 function Header() {
   return (
-    <div>
-        <h1>Header</h1>
+    <div className="mainDiv">
+        <div className='leftDiv'>
+            <ul>
+              <li><img src={netflixLogo} alt="Logo" /></li>
+              <li>Netflix</li>
+              <li>Home</li>
+              <li>TVShows</li>
+              <li>Movies</li>
+              <li>Latest</li>
+              <li>MyList</li>
+              <li>Browse by Languages</li>
+              <li></li>
+            </ul>
+        </div>
+
+        <div className='rightDiv'>
+            <ul>
+              <li><SearchOutlinedIcon /></li>
+              <li><NotificationsNoneOutlinedIcon /></li>
+              <li><AccountBoxOutlinedIcon /></li>
+              <li><ArrowDropDownOutlinedIcon /></li>
+            </ul>
+        </div>
     </div>
   )
 }
