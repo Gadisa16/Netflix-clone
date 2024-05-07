2319da87387de5ddeff5ede9effba57e068f904e
Gadisa16 <133113137+Gadisa16@users.noreply.github.com>
2024-05-07 13:40:41 +0300

    rows is added as one component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../../components/header/Header.jsx';"../../components/header/Header.jsx";
 import Footer from "../../components/footer/Footer.jsx";
 import Banner from '../../components/banner/Banner.jsx';
+import RowList from '../../components/rows/rowList/RowList.jsx';
 
 
 function Home() {
@@ -9,6 +10,7 @@ function Home() {
     <>
       <Header />
       <Banner />
+      <RowList />
       <Footer />
     </>
   )
