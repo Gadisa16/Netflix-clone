c39f422c773d456fa72c76c20dc10cf1c81e5afb
Gadisa16 <133113137+Gadisa16@users.noreply.github.com>
2024-10-26 11:48:34 +0300

    I have added google analytics just to practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect} from 'react';
 import "./app.css";
 import Home from "./pages/home/Home.jsx";
+import ReactGA from 'react-ga4';
 
 function App() {
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+
+    // Optional: Enable IP anonymization
+    ReactGA.set({ anonymizeIp: true });
+    return () => {
+      ReactGA.abort();
+    };
+  }, []);
+
+
   return (
     <div className="App">
       <Home />
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-// import './main.css'
+// main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize(import.meta.env.VITE_GOOGLE_ANALYTICS_ID);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
-)
+);
